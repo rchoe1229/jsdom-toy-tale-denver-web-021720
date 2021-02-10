0e8cdbec2825b3fff90364899a9368b2c3c31233
rchoe1229 <ryan.choe@outlook.com>
2021-02-10 12:01:18 -0700

    Up to Fetch with Options

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let addToy = false;
+const indexUrl = 'http://localhost:3000/toys'
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +15,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch(indexUrl)
+  .then(res => res.json())
+  .then(toys => renderToys(toys))
+
+const renderToys = (toys) => {
+  toys.forEach(toy => {
+    const toyCollection = document.querySelector('#toy-collection')
+
+    const toyCard = document.createElement('div')
+      toyCard.className = 'card'
+
+    const cardTitle = document.createElement('h2')
+      cardTitle.innerText = toy.name
+
+    const cardImage = document.createElement('img')
+      cardImage.className = 'toy-avatar'
+      cardImage.src = toy.image
+
+    const cardLikes = document.createElement('p')
+      cardLikes.innerText = `${toy.likes} likes`
+
+    const likeButton = document.createElement('button')
+      likeButton.className = 'like-btn'
+      likeButton.textContent = 'Like <3'
+
+      toyCard.append(cardTitle, cardImage, cardLikes)
+      toyCollection.append(toyCard)
+    })
+
+
+    
+}
